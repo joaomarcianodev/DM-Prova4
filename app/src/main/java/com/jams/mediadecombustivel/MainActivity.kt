b6a1a2fe6a2c6b13991f6dd144344811c241505f
JoaoAugusto2020 <joaoaugustoptc2020@gmail.com>
2025-11-09 12:33:37 -0300

    Initial files

---
+++ app/src/main/java/com/jams/mediadecombustivel/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jams.mediadecombustivel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jams.mediadecombustivel.ui.theme.MédiaDeCombustívelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MédiaDeCombustívelTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MédiaDeCombustívelTheme {
+        Greeting("Android")
+    }
+}
